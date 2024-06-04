f184be42cd9ace07d895508fe3dccd838d2c987d
Mohd Rafey Khan <rafey.khan@unthinkable.co>
2024-06-04 07:55:55 +0530

    docker container setupped done

--- real-time-streaming/dags/kafka_stream.py
+++ real-time-streaming/dags/kafka_stream.py
@@ -1,12 +1,12 @@
-import uuid
-from datetime import datetime
-from airflow import DAG
-from airflow.operators.python import PythonOperator
+# import uuid
+# from datetime import datetime
+# from airflow import DAG
+# from airflow.operators.python import PythonOperator
 
-default_args = {
-    'owner': 'airscholar',
-    'start_date': datetime(2023, 9, 3, 10, 00)
-}
+# default_args = {
+#     'owner': 'airscholar',
+#     'start_date': datetime(2023, 9, 3, 10, 00)
+# }
 
 def get_data():
     import requests
@@ -57,12 +57,15 @@ def stream_data():
             logging.error(f'An error occured: {e}')
             continue
 
-with DAG('user_automation',
-         default_args=default_args,
-         schedule_interval='@daily',
-         catchup=False) as dag:
 
-    streaming_task = PythonOperator(
-        task_id='stream_data_from_api',
-        python_callable=stream_data
-    )
+stream_data()
+
+# with DAG('user_automation',
+#          default_args=default_args,
+#          schedule_interval='@daily',
+#          catchup=False) as dag:
+
+#     streaming_task = PythonOperator(
+#         task_id='stream_data_from_api',
+#         python_callable=stream_data
+#     )
